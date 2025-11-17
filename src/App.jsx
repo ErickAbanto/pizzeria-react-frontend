c280a6ff9de3dbddd8ba42800070e896f1d06dc4
ErickAbanto <aeriatab@gmail.com>
2025-11-17 15:38:48 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl text-blue-500 ">
+        Hola Mundo
+      </h1>
+
+
+
+    </>
+  )
+}
+
+export default App
